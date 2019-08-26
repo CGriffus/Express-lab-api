82893186e3d45fe1f7eb5226949a51c75b0c9659
Caitlin <caitlin@griffus.com>
2019-08-26 08:53:21 -0400

    created server with shop items

---
+++ routes/cart.js
@@ -0,0 +1,10 @@
+const cart = [
+  { name: "Helm of the Inquisitor", price: 200, category: "armor", id: "1a" },
+  { name: "Bianca", price: 100000, category: "rare weapon", id: "1rw" },
+  { name: "Health Potion", price: 5, category: "potion", id: "1p" },
+  { name: "Lyrium Potion", price: 5, category: "potion", id: "2p" },
+  { name: "Iron Round Shield", price: 50, category: "armor", id: "2a" },
+  { name: "Apprentice Mage Robes", price: 40, category: "armor", id: "3a" }
+];
+
+module.exports = cart;
