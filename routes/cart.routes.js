4a69ecb6752107c5a4c6e5a85fadd106953ac244
Caitlin <caitlin@griffus.com>
2019-08-28 14:10:52 -0400

    routes working in postman

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -10,16 +10,34 @@ function selectAllItems(req, res) {
 
 cartRoutes.get("/cart-items", selectAllItems);
 
-// cartRoutes.post("/cart-items", (req, res) => {
-//   console.log(req.body);
-// });
+cartRoutes.post("/cart-items", (req, res) => {
+  pool
+    .query(
+      "insert into ShoppingCart (product, price, quantity) values ($1::text, $2::int, $3::int)",
+      [req.body.product, req.body.price, req.body.quantity]
+    )
+    .then(() => {
+      selectAllItems(req, res);
+    });
+});
 
-// cartRoutes.put("/cart-items/:id", (req, res) => {
-//   console.log(req.params.id, req.body);
-// });
+cartRoutes.put("/cart-items/:id", (req, res) => {
+  pool
+    .query(
+      "update ShoppingCart set product=$1::text, price=$2::int, quantity=$3::int where id=$4::int",
+      [req.body.product, req.body.price, req.body.quantity, req.params.id]
+    )
+    .then(() => {
+      selectAllItems(req, res);
+    });
+});
 
-// cartRoutes.delete("/cart-items/:id", (req, res) => {
-//   console.log(req.params.id);
-// });
+cartRoutes.delete("/cart-items/:id", (req, res) => {
+  pool
+    .query("delete from ShoppingCart where id=$1::int", [req.params.id])
+    .then(() => {
+      selectAllItems(req, res);
+    });
+});
 
 module.exports = cartRoutes;
